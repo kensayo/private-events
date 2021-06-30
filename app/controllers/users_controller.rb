030a8ea54775c83d9b5c8ad5672942a806d2472b
Kenny Ortega <kennyaoc@gmail.com>
2021-06-30 02:10:06 -0400

    Fix last linter errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,4 @@
 class UsersController < ApplicationController
-  def index
-    id = current_user.id
-    @event = Event.where(user_id = '2')
-    p @event.name
-  end
-
   def show
     @past_events = Event.attend_past(current_user.id)
     @upcoming_events = Event.attend_upcoming(current_user.id)
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,13 +1,4 @@
 class Attendance < ApplicationRecord
   belongs_to :user
   belongs_to :event
-  validate :validate_attend
-
-  private
-
-  def validate_attend
-    if Attendance.where(user_id: user_id).where(event_id: event_id).count == 1
-      errors.add :user_id, 'You have already mark presence in this event'
-    end
-  end
 end
